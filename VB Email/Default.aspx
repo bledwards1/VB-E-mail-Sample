486fb1dede21b5ac11ac118f0abfcf895929ab66
Brad Edwards <bledwards1@gmail.com>
2016-10-06 15:10:29 -0500

    Copying from an old account

---
+++ VB Email/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!--
+Written by Brad Edwards
+6/14/2014
+http://tomballpc.com
+  -->
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Click the button to send an e-mail in Visual Basic!</div>
+        <asp:Button ID="Button1" runat="server" Text="Click Me" />
+    </form>
+</body>
+</html>
---
+++ VB Email/Default.aspx.vb
@@ -0,0 +1,28 @@
+﻿
+' Written by Brad Edwards
+' 6/14/2014
+' http://tomballpc.com
+
+
+' Be sure to import this
+
+Imports System.Net.Mail
+
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim myMessage As MailMessage = New MailMessage()
+        myMessage.Subject = "Test Message" ' Enter the subject here
+        myMessage.Body = "Hello world, from TomballPC.com" ' Message body
+        myMessage.From = New MailAddress("youremail@yourdomain.com", "Enter your name")
+        myMessage.To.Add(New MailAddress("youremail@yourdomain.com", "Enter recipient name"))
+
+        Dim mySmtpClient As SmtpClient = New SmtpClient()
+        mySmtpClient.Send(myMessage)
+    End Sub
+End Class
